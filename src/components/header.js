9820d1c865fb1201e78c3886d8f06f09c1a53c2b
ramesh <rameshsolaimalai@gmail.com>
2020-07-08 11:08:48 +0530

    design changes

--- src/components/header.js
+++ src/components/header.js
@@ -10,15 +10,14 @@ const Header = () =>{
             <header>
                 <nav className="nav-wrapper nav">
                     <div className="container">
-                        <span className="brand-logo left">CHAT</span>
                             {/* <span className="sidenav-trigger" data-target="mobile-links">
                             <i className="material-icons hide-on-large-only">menu</i>
-                            </span> */}                            <ul className="right ">
+                            </span> */}                            <ul className="right">
                             
                             <li><Link to="/friends">Friend</Link></li>
                             <li><Link to="/requests">Requests</Link></li>
                             <li><Link to="/search">Add Friend</Link></li>
-                            <li><Link to="/logout">Logout</Link></li>
+                            <li><Link to="/logout"><i className="material-icons" style={{fontSize:"1.2em"}}>lock</i></Link></li>
                         </ul>
                     </div>
                 </nav>
