7e9573a0764a6eccdd0ab3e9892fabd65c67ee2a
Ramesh Solaimalai <67944819+ramesh878-dj@users.noreply.github.com>
2020-07-09 10:29:23 +0530

    Update chat.js

--- src/components/chat.js
+++ src/components/chat.js
@@ -35,7 +35,7 @@ class Chat extends Component {
         // const socketPath = 'ws://dochatservice.herokuapp.com/ws/chating/'+roomName+'/';
 
         var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-        const chatSocket = new ReconnectingWebSocket(
+        const chatSocket = new WebSocket(
             'wss://dochatservice.herokuapp.com:30083/ws/chating/new/'
         );
 
